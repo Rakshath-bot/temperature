ffe902a1e0a69b0c54bace5250e3fdfbf84db3da
rakshath <rakshathtp123@gmail.com>
2026-02-18 01:34:01 +0530

    added project with code visualisation

---
+++ project.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x_data = [0.10,0.20,0.30,0.40,0.50,0.60,0.70,0.80,0.90]
+y_data = np.exp(x_data)
+plt.plot(x_data, y_data)
+plt.title('Temperature Rise')
+plt.xlabel('X_data, Time')
+plt.ylabel('Y_data, Temperature')
+plt.show()
+categories = ['Category A', 'Category B', 'Category C', 'Category D']
+values = [23, 45, 56, 78]
+
+# Create the bar plot
+plt.bar(categories, values, color='skyblue')
+
+# Add titles and labels
+plt.title('Bar Plot Example')
+plt.xlabel('Categories')
+plt.ylabel('Values')
+
+# Display the plot
+plt.show()
